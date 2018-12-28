95a35c9ea5b76c6516ef52bf5cb7e1b979b772f0
gaosikang <498039142@qq.com>
2018-12-28 19:12:42 +0800

    update cfg

--- README.md
+++ README.md
@@ -1,8 +1,46 @@
-## Test Project...
+## TUTU-h5
+
+图图教育。
 
 ### 安装依赖
 ```bash
-
 $ npm install
 
+```
+
+### 启动项目
+```bash
+$ npm run dev
+
+```
+
+### 打包项目
+```bash
+$ npm run build
+
+```
+
+-----------------------------------------------
+
+<font color=red>关于gulp插件打版本号的问题</font>
+<font color=red>*(不推荐更新gulp-rev、gulp-rev-collector插件)*</font>
+
+##### 打开node_modules\gulp-rev\node_modules\rev-path\index.js
+
+```
+  第133行 manifest[originalFile] = revisionedFile;
+  更新为: manifest[originalFile] = originalFile + '?v=' + file.revHash;
+```
+
+##### node_modules\gulp-rev\node_modules\rev-path\index.js
+
+```
+  10行 return filename + '-' + hash + ext;
+  更新为: return filename + ext;
+```
+##### node_modules\gulp-rev-collector\index.js
+
+```
+  第40行 let cleanReplacement =  path.basename(json[key]).replace(new RegExp( opts.revSuffix ), '' );
+　更新为 let cleanReplacement =  path.basename(json[key]).split('?')[0];
 ```
--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,22 @@
-let gulp = require('gulp');
-let minifycss = require('gulp-minify-css');
-let uglify = require('gulp-uglify');
-let babel = require('gulp-babel');
-let changed = require('gulp-changed');
-let htmlmin = require('gulp-htmlmin');
-let debug = require('gulp-debug');
+const gulp = require('gulp');
+const clean = require('gulp-clean');
+const minifycss = require('gulp-minify-css');
+const uglify = require('gulp-uglify');
+const babel = require('gulp-babel');
+const changed = require('gulp-changed');
+const htmlmin = require('gulp-htmlmin');
+const debug = require('gulp-debug');
+const imagemin = require('gulp-imagemin');
+const runSequence = require('gulp-run-sequence'); // 任务同步队列
+const cache = require('gulp-cache'); // 压缩修改的图片
+const pngquant = require('imagemin-pngquant-gfw'); // 深度压缩png
 
-let htmlminOptions = {
+// 添加版本号
+const rev = require('gulp-rev');
+const revFormat = require('gulp-rev-format');
+const revCollector = require('gulp-rev-collector');
+
+const htmlminOptions = {
   removeComments: true, //清除HTML注释
   collapseWhitespace: true, //压缩HTML
   collapseBooleanAttributes: true, //省略布尔属性的值 <input checked="true"/> ==> <input />
@@ -17,21 +27,26 @@ let htmlminOptions = {
   minifyCSS: true //压缩页面CSS
 };
 
+/*=====================copy-js==========================*/
 gulp.task('copy-jslib', function() {
   return gulp.src('src/js/lib/**/*').pipe(gulp.dest('dist/js/lib/'));
 });
 
-gulp.task('copy-img', function() {
-  return gulp.src('src/img/**/*').pipe(gulp.dest('dist/img/'));
-});
-
+/*=====================copy静态文件==========================*/
 gulp.task('copy', function() {
-  gulp.start('copy-img', 'copy-jslib');
+  gulp.start('copy-jslib');
 });
 
-// 压缩html
+/*=====================清除dist==========================*/
+gulp.task('clean', function() {
+  return gulp
+    .src('./dist')
+    .pipe(clean());
+});
+
+/*=====================压缩html==========================*/
 gulp.task('html', function() {
-  let srcHtml = 'src/pages/*.html';
+  let srcHtml = 'src/pages/**/*.html';
   let destHtml = 'dist/pages';
   return gulp
     .src(srcHtml)
@@ -41,9 +56,9 @@ gulp.task('html', function() {
     .pipe(gulp.dest(destHtml));
 });
 
-// 压缩css
+/*=====================压缩css==========================*/
 gulp.task('css', function() {
-  let srcCss = 'src/css/*.css';
+  let srcCss = 'src/css/**/*.css';
   let destCss = 'dist/css';
   return (
     gulp
@@ -55,9 +70,21 @@ gulp.task('css', function() {
   );
 });
 
-// 压缩js
+/*=====================压缩图片==========================*/
+gulp.task('img', function() {
+  gulp.src('src/img/*.{png,jpg,gif,ico}')
+    .pipe(cache(imagemin({
+      optimizationLevel: 4,
+      progressive: true, // 默认：false 无损压缩jpg
+      svgoPlugins: [{removeViewBox: false}],
+      use: [pngquant()]
+    })))
+    .pipe(gulp.dest('dist/img'));
+});
+
+/*=====================压缩js==========================*/
 gulp.task('js', function() {
-  let srcpath = 'src/js/*.js';
+  let srcpath = 'src/js/**/*.js';
   let destpath = 'dist/js/';
   return gulp
     .src(srcpath)
@@ -70,6 +97,7 @@ gulp.task('js', function() {
     )
     .pipe(
       uglify({
+        compress: true,
         mangle: { except: ['requirejs', 'require', 'exports', 'module', '$'] } //排除混淆关键字
       }).on('error', function(e) {
         console.log(e);
@@ -78,6 +106,41 @@ gulp.task('js', function() {
     .pipe(gulp.dest(destpath));
 });
 
+/*=====================生成版本号清单==========================*/
+gulp.task('rev', function() {
+  return gulp.src(['dist/css/**/*.css', 'dist/js/**/*.js', '!dist/js/lib/*.js'])
+    .pipe(debug({ title: '【生成版本文件】：' }))
+    .pipe(rev())
+    .pipe(revFormat({
+      prefix: '.', // 在版本号前增加字符
+      suffix: '.cache', // 在版本号后增加字符
+      lastExt: true
+    }))
+    .pipe(rev.manifest())
+    .pipe(gulp.dest('dist/config/'));
+});
+
+/*=====================路径替换==========================*/
+gulp.task('version', function() {
+  return gulp.src(['dist/config/*.json', 'dist/**/*.html'])
+    .pipe(debug({ title: '【添加版本号】：' }))
+    .pipe(revCollector()) // 根据manifest.json进行文件的替换
+    .pipe(gulp.dest('dist/'));
+});
+
+/*=====================上线打包==========================*/
+gulp.task('build', function(done) {
+  runSequence(
+    ['clean'],  // 1.清空dist目录
+    ['copy'],   // 2.copy静态资源
+    ['html', 'img', 'css', 'js'], // 3.打包文件
+    ['rev'],     // 4.生成版本
+    ['version'], // 5.打版本
+    done
+  );
+});
+
+/*=====================文件打包==========================*/
 gulp.task('default', function() {
-  gulp.start('copy', 'html', 'css', 'js');
+  gulp.start('html', 'img', 'css', 'js');
 });
